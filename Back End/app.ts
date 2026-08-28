330907add4578866ef5a869f0bdf52f93d348006
Hafiz Muhammad Ahmad <h.m.ahmad.2024@gmail.com>
2026-08-28 18:04:36 +0500

    Refactor app.ts for improved structure and clarity

--- Back End/app.ts
+++ Back End/app.ts
@@ -91,7 +91,7 @@ app.use((req: Request, res: Response) => {
   res.status(404).json({ message: 'Resource not found', path: req.originalUrl });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
